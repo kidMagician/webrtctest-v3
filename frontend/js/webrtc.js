4f817ecb68f1a7f045e68cef88e709058992986c
nss <sungsuk.nam@smoothy.co>
2018-09-05 11:27:24 +0900

    before pcClinet module

--- frontend/js/webrtc.js
+++ frontend/js/webrtc.js
@@ -5,34 +5,39 @@ var room;
 const BROADCASTMESSAGE ={
       ENTER_ROOM:"broadcast:enterRoom",
       LEAVE_ROOM:"broadcast:leaveRoom"
-    }
+}
+
+const PARTICIPANTMESSAGE ={
+      DISCONNECTED:"participant:disconnected",
+      CONNECTED:"participant:connected"
+}
     
-    const NEGOTIATION_MESSAGE ={
+const NEGOTIATION_MESSAGE ={
       OFFER:"negotiation:offer",
       ANSWER:"negotiation:answer",
       CANDIDATE:"negotiation:candidate",
       SUCESS_NEGOTIATION:"negotiation:sucess",
       FAILED_NEGOTIATION:"negotiation:failed"
-    }
+}
     
-    const ROOM_MESSANGE ={
+const ROOM_MESSANGE ={
       CONNECT_ROOM:"room:connectRoom",
       FAILED_CONNECT_ROOM:"room:failedEnterRoom",
       LEAVE_ROOM:"room:leaveRoom",
-    }
+}
 
-    const FAILED_CREATE_STATE={
+const FAILED_CREATE_STATE={
       DUPLIACTED_NAME:"createRoomFailed:duplicatedname",
       TOO_LONG_NAME:"createRoomFailed:tooLongName",
       TOO_SHORT_NAME:"createRoomFailed:tooShortName",
       FORBIDDEN_NAME:"createRoomFailed:forbiddenname"
 
-    }
+}
     
-    const SESSION_MESSAGE ={
+const SESSION_MESSAGE ={
       LOGIN: "session:login",
       LOGOUT: "session:logout"
-    }
+}
 
 function initRtcPeerConnection(otherUsername,remoteVideo,callback){
 
@@ -72,7 +77,6 @@ module.exports.connect = function(){
 
       })
 
-
 }
 
 ws.onopen = function () { 
@@ -90,46 +94,56 @@ ws.onmessage = function (msg) {
       var data = JSON.parse(msg.data); 
          
       switch(data.type) { 
-         
-         case SESSION_MESSAGE.LOGIN: 
-               handleLogin(data.success); 
-               break; 
-         case SESSION_MESSAGE.LOGOUT: 
-               handleLogout();  
-               break;       
-         case "authenticate":
-               handleAuthenticate(data.success);
-               break;
-         case ROOM_MESSANGE.CREATE_ROOM:
-               handleCreateRoom(data.success)
-               break;
-         case ROOM_MESSANGE.ENTER_ROOM:
-               handleEnterRoom(data.users);
-               break;
-         case ROOM_MESSANGE.LEAVE_ROOM:
-               handleLeaveRoom();
-               break;
-         case NEGOTIATION_MESSAGE.OFFER: 
-               handleOffer( data.fromUsername,data.offer); 
-               break; 
-         case NEGOTIATION_MESSAGE.ANSWER: 
-               handleAnswer(data.fromUsername,data.answer); 
-               break; 
-         case NEGOTIATION_MESSAGE.CANDIDATE: 
-               handleCandidate(data.fromUsername,data.candidate); 
-               break; 
-   
-         case BROADCASTMESSAGE.LEAVE_ROOM:
-               handleBroadcastLeaveRoom(data.username);
-               break;
+            case ROOM_MESSANGE.CONNECT_ROOM:
+                  handleConnectRoom()            
+            break;
+
+            case ROOM_MESSANGE.CREATE_ROOM:
+                  handleCreateRoom(data.success)
+            break;
+
+            case ROOM_MESSANGE.ENTER_ROOM:
+                  handleEnterRoom(data.users);
+            break;
+
+            case ROOM_MESSANGE.LEAVE_ROOM:
+                  handleLeaveRoom();
+            break;
+
+            case NEGOTIATION_MESSAGE.OFFER: 
+                  handleOffer( data.fromUsername,data.offer); 
+            break;
+
+            case NEGOTIATION_MESSAGE.ANSWER: 
+                  handleAnswer(data.fromUsername,data.answer); 
+            break;
+
+            case NEGOTIATION_MESSAGE.CANDIDATE: 
+                  handleCandidate(data.fromUsername,data.candidate); 
+            break; 
+
+            case PARTICIPANTMESSAGE.DISCONNECTED:
+                  handleParticipantDisconnected(data.username);
+            break;
+
+            case PARTICIPANTMESSAGE.CONNECTED:
+                  handleParticipantConnected();
+            break;
                      
          default: 
             break; 
       }
-   };
-   
-handleLeaveRoom()
+};
 
+function handleParticipantDisconnected(){
+
+      room.emit('participantDisconnected')
+}
+
+
+function handleLeaveRoom(){
+      room.emit('disconnected',room)
+}
 
 
 
